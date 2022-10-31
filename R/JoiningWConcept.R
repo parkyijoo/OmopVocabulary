1cc9db7b97e569206fcf5b46c5ff2881e29d5601
Yijoo Park <yijoo0320@gmail.com>
2022-10-31 15:13:37 +0900

    10.31 update

--- R/JoiningWConcept.R
+++ R/JoiningWConcept.R
@@ -1,6 +1,9 @@
+## Set .Renviron to hide important information
 # install.packages("usethis")
-# install.packages("rlang")
 # usethis::edit_r_environ()
+
+## Install required packages ##
+# install.packages("rlang")
 # install.packages("dplyr") 
 # install.packages("openxlsx") 
 # install.packages("readr")
@@ -8,6 +11,7 @@ library(rlang)
 library(dplyr)
 library(openxlsx)
 library(readr)
+
 dataFolder <- file.path(Sys.getenv("gitFolder"), "dr-you-group/OmopVocabulary/data")
 
 # 언어 삭제/변경(인코딩 오류)
@@ -15,70 +19,85 @@ Sys.setlocale("LC_ALL", "C")
 Sys.setlocale("LC_ALL", "Korean")
 
 DRUG <- read.csv(
-  file = "C:/Users/yijoo0320/git/dr-you-group/OmopVocabulary/data/drug/drug.csv",
+  file = file.path(dataFolder, "drug/drug.csv"),
   header = TRUE, sep = ",")
 
 DRUG_HIRA <- read.csv(
-  file = "C:/Users/yijoo0320/git/dr-you-group/OmopVocabulary/data/drug/drughira.csv",
+  file = file.path(dataFolder, "drug/drughira.csv"),
   header = TRUE, sep = ",")
 
-# 조인키 열이름 맞추기
+## export the concept of OmopVoca ##
+concept <- read.csv(file.path(dataFolder, "OmopVoca2022.10.27/CONCEPT.csv"), 
+                    quote = "",
+                    row.names = NULL, sep = "\t") #,nrows=10
+
+# Join by source_code
 names(DRUG_HIRA)
 names(DRUG_HIRA) <- c("researcher","code","source_code","concept_id_hira", "local_name_hira", "concept_name_hira", "matching", "concept_id_kids", "concept_name_kids", "final_concept_id", "X", "X.1", "final_concept_name", "comment")
 
-# source_code로 조인
 DRUG_HIRA$source_code <- as.character(DRUG_HIRA$source_code)
 JOIN <- left_join(DRUG, DRUG_HIRA, by = "source_code")
-
-# DRUG 필요 열만 추출
 colnames(JOIN)
 JOIN <- JOIN[,c(1,2,3,4,5,6,7,8,9,10,11,20,24)]
 
-# target_concept_id 변경없는 경우, NA로 통일
-JOIN$final_concept_id = ifelse(
-  JOIN$target_concept_id == JOIN$final_concept_id,
+
+# seperate data to complete/incomplete data
+CompleteData <- JOIN  
+IncompleteData <- JOIN
+
+## Complete ##
+CompleteData$final_concept_id = ifelse(
+  CompleteData$target_concept_id == CompleteData$final_concept_id,
   NA,
-  JOIN$final_concept_id
+  CompleteData$final_concept_id
 )
 
-# UPDATE: 업데이트 데이터 + 업데이트와 무관한 전체 데이터 / PREUPDATE: 업데이트 전 데이터 
-UPDATE <- JOIN  
-PREUPDATE <- JOIN
-
 # NA 들어있는 행 삭제
-is.na(PREUPDATE$comment) = 0
-PREUPDATE <- na.omit(PREUPDATE)
+CompleteData$comment <- ifelse(
+  is.na(CompleteData$comment),
+  0,
+  CompleteData$comment
+)
+CompleteData <- na.omit(CompleteData)
 
-# 업데이트 전 테이블에 invalid_reason "U" 작성
-PREUPDATE$invalid_reason <- "U"
-UPDATE$invalid_reason <- NA
+# invalid_reason NA
+CompleteData$invalid_reason <- NA
 
 # final_concept_id가 NA가 아니라면, final_concept_id 값을 target_concept_id 에 할당
-UPDATE$target_concept_id <- ifelse(
-  is.na(UPDATE$final_concept_id),
-  UPDATE$target_concept_id,
-  UPDATE$final_concept_id
+CompleteData$target_concept_id <- ifelse(
+  is.na(CompleteData$final_concept_id),
+  CompleteData$target_concept_id,
+  CompleteData$final_concept_id
 )
 
 # valid_start_date 작성
-UPDATE$valid_start_date <- ifelse(
-  is.na(UPDATE$final_concept_id),
-  "2021-03-06",
-  "2022-10-20"
-)
+CompleteData$valid_start_date <- "2022-10-20"
 
 # valid_end_date 작성
-UPDATE$valid_end_date <- ifelse(
-  is.na(UPDATE$final_concept_id),
-  "2022-10-19",
+CompleteData$valid_end_date <- NA
+
+## Incomplete ##
+IncompleteData$final_concept_id <- ifelse(
+  union(CompleteData$target_concept_id = IncompleteData$target_concept_id,
+        IncompleteData$final_concept_id = NA),
+  NA,
+  IncompleteData$final_concept_id
+)
+
+IncompleteData$invalid_reason <- ifelse(
+  IncompleteData$final_concept_id != IncompleteData$target_concept_id,
+  "U",
   NA
 )
 
-readr::write_tsv(x = PREUPDATE, file = file.path(dataFolder, "CompleteMapping2022.10.27.csv"),  na = "")
-readr::write_tsv(x = UPDATE, file = file.path(dataFolder, "IncompleteMapping2022.10.27.csv"),  na = "")
+# valid_start_date 작성
+IncompleteData$valid_start_date <- "2021-03-06"
+
+# valid_end_date 작성
+IncompleteData$valid_end_date <- "2022-10-19"
 
 # concept_id 변경과 변경없음 합체
-conMap <- rbind(PREUPDATE, UPDATE)
+conMap <- rbind(IncompleteData, CompleteData)
 
 # 제품명 오름차순, 동일 시 시작일 오름차순
 conMap[order(conMap$source_code_description, conMap$valid_start_date),]
@@ -86,24 +105,13 @@ conMap[order(conMap$source_code_description, conMap$valid_start_date),]
 colnames(conMap)
 conMap <- conMap[,c(1,2,3,4,5,6,7,8,9,10,13)]
 
-# 파일 tsv로 저장
-readr::write_tsv(x = conMap, file = file.path(dataFolder, "SourceToConceptMap2022.10.27.csv"))
-
-#======================================================================================
-concept <- read.csv(file.path(dataFolder, "OmopVoca2022.10.25/CONCEPT.csv"), sep = "\t") #,nrows=10
-conMap <- readr::read_tsv(file = file.path(dataFolder, "SourceToConceptMap2022.10.27.csv"))
-
-# source_code와 concept_code 일치여부 확인 
-conMap[which(conMap$source_code %in% concept$concept_code == FALSE),]
-
-# source_concept_id와 concept_id 일치여부 확인(모두 일치)
-conMap[which(conMap$source_concept_id %in% concept$concept_id == FALSE),]
 
 # 조인키 열이름 맞추기
 names(concept)
 names(concept) <- c("source_concept_id", "concept_name", "domain_id", "vocabulary_id", "concept_class_id", "standard_concept", "concept_code", "valid_start_date", "valid_end_date",   "invalid_reason")
 
 # source_concept_id로 조인
+conMap$source_concept_id <- as.integer(conMap$source_concept_id)
 concept$source_concept_id <- as.double(concept$source_concept_id)
 union <- left_join(conMap, concept, by = "source_concept_id")
 
@@ -121,6 +129,7 @@ conMap$comment <- union$comment
 
 # target_vocabulary_id에 vocabulary_id 작성
 names(concept) <- c("target_concept_id", "concept_name", "domain_id", "vocabulary_id", "concept_class_id", "standard_concept", "concept_code", "valid_start_date", "valid_end_date",   "invalid_reason")
+conMap$target_concept_id <- as.double(conMap$target_concept_id)
 concept$target_concept_id <- as.double(concept$target_concept_id)
 union <- left_join(conMap, concept, by = "target_concept_id")
 conMap$target_vocabulary_id <- union$vocabulary_id
@@ -131,6 +140,8 @@ conMap$target_code_description <- union$concept_name
 # target_concept_domain 추가
 conMap$target_concept_domain <- union$domain_id
 
+CompleteData <-  conMap[conMap$valid_start_date == "2022-10-20",]
+
 # 열 순서 변경(concept_class_id, vocabulary_id, source_concept_domain)
 conMap <- conMap %>% relocate(source_concept_class_id, .before = source_vocabulary_id) 
 conMap <- conMap %>% relocate(source_concept_domain, .after = source_vocabulary_id)
@@ -139,11 +150,17 @@ conMap <- conMap %>% relocate(target_code_description, .after = target_concept_d
 conMap <- conMap %>% relocate(comment, .after = invalid_reason)
 
 # NA값 정리
-conMap[conMap$source_concept_id == 0, "source_concept_id"] = NA
-conMap[conMap$source_concept_class_id == "Undefined", "source_concept_class_id"] = NA
-conMap[conMap$source_vocabulary_id == "None", "source_vocabulary_id"] = NA
-conMap[conMap$source_concept_domain == "Metadata", "source_concept_domain"] = NA
+conMap$source_concept_id <- replace(conMap$source_concept_id == 0, NA)
+conMap$source_concept_class_id <- replace(conMap$source_concept_class_id == "Undefined", NA)
+conMap$source_vocabulary_id <- replace(conMap$source_vocabulary_id == "None", NA)
+conMap$source_concept_domain <- replace(conMap$source_concept_domain == "Metadata", NA)
+
+# UpdateCodes
+CompleteData <- conMap[union(which(conMap$invalid_reason == "U"),
+                             which(conMap$valid_start_date == "2022-10-20")),]
 
 write.csv(conMap, file = "C:/Users/yijoo0320/Desktop/conMap.csv", na = "")
-readr::write_tsv(x = conMap, file = file.path(dataFolder, "SourceToConceptMap2022.10.27.csv"),  na = "")
+readr::write_tsv(x = conMap, file = file.path(dataFolder, "SourceToConceptMap/SourceToConceptMap2022.10.31.csv"),  na = "")
+readr::write_tsv(x = CompleteData, file = file.path(dataFolder, "CompleteMapping/CompleteMapping2022.10.31.csv"),  na = "")
+readr::write_tsv(x = IncompleteData, file = file.path(dataFolder, "IncompleteMapping/IncompleteMapping2022.10.31.csv"),  na = "")
 
--- R/UpdateCodes.R
+++ R/UpdateCodes.R
@@ -2,20 +2,49 @@ library(dplyr)
 library(readr)
 dataFolder <- file.path(Sys.getenv("gitFolder"), "dr-you-group/OmopVocabulary/data")
 
-concept <- read.csv(file.path(dataFolder, "OmopVoca2022.10.25/CONCEPT.csv"), sep = "\t") #,nrows=10
-conMap <- readr::read_tsv(file = file.path("./data", "SourceToConceptMap2022.10.27.csv"),  na = "")
-PREUPDATE <- readr::read_tsv(file = file.path("./data", "CompleteMapping2022.10.27.csv"),  na = "")
-UPDATE <- readr::read_tsv(file = file.path("./data", "IncompleteMapping2022.10.27.csv"),  na = "")
+concept <- read.csv(file.path(dataFolder, "OmopVoca2022.10.27/CONCEPT.csv"), sep = "\t") #,nrows=10
+conMap <- readr::read_tsv(file = file.path(dataFolder, "SourceToConceptMap/SourceToConceptMap2022.10.31.csv"),  na = "")
+CompleteData <- readr::read_tsv(file = file.path(dataFolder, "CompleteMapping/CompleteMapping2022.10.31.csv"),  na = "")
+IncompleteData <- readr::read_tsv(file = file.path(dataFolder, "IncompleteMapping/IncompleteMapping2022.10.31.csv"),  na = "")
 
-# code 불일치 행 추출
-codeFalse <- conMap[which(conMap$source_code %in% concept$concept_code == FALSE),]
-View(codeFalse)
+# source_concept_id로 조인
+colnames(concept)[1] <- "target_concept_id"
+concept$target_concept_id <- as.character(concept$target_concept_id)
+CompleteData$target_concept_id <- as.character(CompleteData$target_concept_id)
+unionC <- left_join(CompleteData, concept, by = "target_concept_id")
 
-# 매핑이 필요한 데이터
-incomplete <- 
+##CompleteData##
+# 열 순서 변경(concept_class_id, vocabulary_id, source_concept_domain)
+colnames(unionC)
+unionC <- unionC[,c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)]
+names(unionC) <- c("source_domain_id", "source_code", "source_concept_id", "source_concept_class_id", "source_vocabulary_id", "source_concept_domain", "source_code_description", "target_concept_id" , "target_vocabulary_id", "target_concept_domain", "target_code_description", "valid_start_date", "valid_end_date", "invalid_reason", "comment")
+unionC <- unionC %>% relocate(source_concept_class_id, .before = source_vocabulary_id) 
+unionC <- unionC %>% relocate(source_concept_domain, .after = source_vocabulary_id)
+unionC <- unionC %>% relocate(target_concept_domain, .after = target_vocabulary_id)
+unionC <- unionC %>% relocate(target_code_description, .after = target_concept_domain)
+unionC <- unionC %>% relocate(comment, .after = invalid_reason)
 
-# 매핑 완료한 데이터
-complete <- rbind(PREUPDATE, )
+# NA값 정리
+unionC$source_concept_id <- replace(unionC$source_concept_id == 0, NA)
+unionC$source_concept_class_id <- replace(unionC$source_concept_class_id == "Undefined", NA)
+unionC$source_vocabulary_id <- replace(unionC$source_vocabulary_id == "None", NA)
+unionC$source_concept_domain <- replace(unionC$source_concept_domain == "Metadata", NA)
 
-readr::write_tsv(x = complete, file = file.path(dataFolder, "CompleteMapping2022.10.27.csv"),  na = "")
-readr::write_tsv(x = incomplete, file = file.path(dataFolder, "IncompleteMapping2022.10.27.csv"),  na = "")
+##IncompleteData##
+unionC$target_concept_id <- as.double(unionC$target_concept_id)
+unionC$valid_start_date <- as.Date(unionC$valid_start_date)
+unionC$valid_end_date <- as.Date(unionC$valid_end_date)
+unionI <- conMap[!(conMap$source_code %in% unionC$source_code),]
+
+# NA값 정리
+unionI[unionI$source_concept_id == 0, "source_concept_id"] = NA
+unionI[unionI$source_concept_class_id == "Undefined", "source_concept_class_id"] = NA
+unionI[unionI$source_vocabulary_id == "None", "source_vocabulary_id"] = NA
+unionI[unionI$source_concept_domain == "Metadata", "source_concept_domain"] = NA
+
+# 코드명 오름차순
+unionC <- unionC[order(unionC$source_code_description), ]
+
+readr::write_tsv(x = CompleteData, file = file.path(dataFolder, "CompleteMapping/CompleteMapping2022.10.31.csv"),  na = "")
+readr::write_tsv(x = IncompleteData, file = file.path(dataFolder, "IncompleteMapping/IncompleteMapping2022.10.31.csv"),  na = "")
+
