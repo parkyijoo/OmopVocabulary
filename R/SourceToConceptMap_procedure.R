c1d1bb33a4f9256f845fa8924c6f2ef9025ee9c0
Yijoo Park <yijoo0320@gmail.com>
2022-11-16 14:46:13 +0900

    add synonym, summarizing codes

--- R/SourceToConceptMap_procedure.R
+++ R/SourceToConceptMap_procedure.R
@@ -16,7 +16,7 @@ dataFolder <- file.path(Sys.getenv("gitFolder"), "dr-you-group/OmopVocabulary/da
 
 # 언어 삭제/변경(인코딩 오류)
 Sys.setlocale("LC_ALL", "C") 
-Sys.setlocale("LC_ALL", "en_US.UTF-8")
+Sys.setlocale("LC_ALL", "Korean.utf8")
 
 PROCEDURE <- read.csv(
   file = file.path(dataFolder, "procedure.csv"),
@@ -32,12 +32,27 @@ conMap <- PROCEDURE
 
 # 조인키 열이름 맞추기
 names(concept)
-names(concept) <- c("source_concept_id", "concept_name", "domain_id", "vocabulary_id", "concept_class_id", "standard_concept", "concept_code", "valid_start_date", "valid_end_date",   "invalid_reason")
+names(concept)[1] <- c("source_concept_id")
+names(conMap)[6] <- c("target_concept_id")
+concept <- concept[,c(1,2,3,4,5,7)]
 
 # source_concept_id로 조인
 conMap$source_concept_id <- as.character(conMap$source_concept_id)
 concept$source_concept_id <- as.character(concept$source_concept_id)
 union <- left_join(conMap, concept, by = "source_concept_id")
+
+# source_vocabulary_id 작성
+conMap$source_vocabulary_id <- union$vocabulary_id
+
+# source_domain_id 추가
+conMap$source_domain_id <- union$domain_id
+
+conMap$source_concept_class_id <- union$concept_class_id
+
+conMap <- conMap %>% relocate(source_domain_id, .before = source_vocabulary_id) 
+conMap <- conMap %>% relocate(source_code, .before = source_concept_id) 
+conMap <- conMap %>% relocate(target_code_description, .after = target_vocabulary_id) 
+conMap <- conMap %>% relocate(source_concept_class_id, .after = source_vocabulary_id) 
 
 ## export the concept of OmopVoca ##
 synonym <- read.csv("C:/Users/yijoo0320/git/dr-you-group/OmopVocabulary/data/OmopVoca2022.10.27/CONCEPT_SYNONYM.csv", 
@@ -46,158 +61,39 @@ synonym <- read.csv("C:/Users/yijoo0320/git/dr-you-group/OmopVocabulary/data/Omo
 
 # 조인키 열이름 맞추기
 names(synonym)
-names(synonym) <- c("source_concept_id", "concept_synonym_name", "language_concept_id")
+names(synonym)[1] <- c("source_concept_id")
 
 # source_concept_id로 조인
-union$source_concept_id <- as.character(union$source_concept_id)
+conMap$source_concept_id <- as.character(conMap$source_concept_id)
 synonym$source_concept_id <- as.character(synonym$source_concept_id)
-union <- left_join(union, synonym, by = "source_concept_id")
+union1 <- left_join(conMap, synonym, by = "source_concept_id")
 
-## Complete ##
-
-union$source_vocabulary_id <- union$vocabulary_id
-union$source_concept_class_id <- union$concept_class_id
-colnames(union)
-union <- union[,c(1,2,3,4,5,6,7,8,9,10,11,21)]
-
-union1 <- union
-union1$target_concept_id_1 = ifelse(
-  union1$target_concept_id_1 == 0,
-  NA,
-  union1$target_concept_id_1
-)
-
-union1$target_vocabulary_id = ifelse(
-  is.na(union1$target_vocabulary_id),
-  0,
-  union1$target_vocabulary_id
-)
-
-union1$invalid_reason.x = ifelse(
-  is.na(union1$invalid_reason.x),
-  0,
-  union1$invalid_reason.x
-)
+conMap <- union1
+conMap <- conMap[,-c(14)]
+names(conMap)[13] <- c("source_concept_synonym")
+conMap <- conMap %>% relocate(source_concept_synonym, .before = source_code_description) 
 
 
-union1$concept_synonym_name = ifelse(
-  is.na(union1$concept_synonym_name),
-  0,
-  union1$concept_synonym_name
-)
+# target_vocabulary_id에 vocabulary_id 작성
+conMap$target_concept_id <- as.character(conMap$target_concept_id)
+names(concept)[1] <- c("target_concept_id")
+union1 <- left_join(conMap, concept, by = "target_concept_id")
 
-# NA 들어있는 행 삭제
-union1 <- na.omit(union1)
-CompleteData <- na.omit(union1)
+# target_vocabulary_id 작성
+conMap$target_vocabulary_id <- union1$vocabulary_id
 
-# invalid_reason NA
-colnames(CompleteData)
-names(CompleteData)<- c("source_domain_id", "source_code", "source_concept_id", "source_vocabulary_id",  "source_code_description", "target_concept_id", "target_vocabulary_id", "valid_start_date", "valid_end_date", "invalid_reason", "seq","source_concept_class_id")
+conMap$target_code_description <- union1$concept_name
 
-# valid_start_date 작성
-CompleteData$valid_start_date <- "2022-10-20"
+# target_domain_id 추가
+conMap$target_domain_id <- union1$domain_id
 
-# valid_end_date 작성
-CompleteData$valid_end_date <- NA
+# target_concept_class_id 추가
+conMap$target_concept_class_id <- union1$concept_class_id
 
+conMap <- conMap %>% relocate(target_domain_id, .before = target_vocabulary_id) 
+conMap <- conMap %>% relocate(target_concept_class_id, .after = target_vocabulary_id)
+conMap <- conMap %>% relocate(target_code_description, .after = target_concept_class_id) 
 
-# 조인키 열이름 맞추기
-names(concept)
-names(concept) <- c("target_concept_id", "concept_name", "domain_id", "vocabulary_id", "concept_class_id", "standard_concept", "concept_code", "valid_start_date", "valid_end_date",   "invalid_reason")
+write.csv(conMap,file = "C:/Users/yijoo0320/git/dr-you-group/OmopVocabulary/data/procedure/SourceToConceptMap_procedure2022.11.16.csv", fileEncoding = "euc-kr", row.names = FALSE, na = "")
 
-
-# target_concept_id로 조인
-CompleteData$target_concept_id <- as.character(CompleteData$target_concept_id)
-concept$target_concept_id <- as.character(concept$target_concept_id)
-union1 <- left_join(CompleteData, concept, by = "target_concept_id")
-
-CompleteData$target_vocabulary_id <- union1$vocabulary_id
-CompleteData$target_code_description <- union1$concept_name
-CompleteData$target_domain_id <- union1$domain_id
-
-# 열 순서 변경(concept_class_id, vocabulary_id, source_concept_domain)
-CompleteData <- CompleteData %>% relocate(source_concept_class_id, .before = source_vocabulary_id) 
-CompleteData <- CompleteData %>% relocate(source_domain_id, .after = source_vocabulary_id)
-CompleteData <- CompleteData %>% relocate(target_domain_id, .after = target_vocabulary_id)
-CompleteData <- CompleteData %>% relocate(target_code_description, .after = target_domain_id)
-CompleteData <- CompleteData %>% relocate(concept_synonym_name, .before = source_code_description) 
-
-
-
-## Incomplete ##
-
-union$target_concept_id_1 = ifelse(
-  union$target_concept_id_1 != 0,
-  NA,
-  union$target_concept_id_1
-)
-
-union$target_vocabulary_id = ifelse(
-  is.na(union$target_vocabulary_id),
-  0,
-  union$target_vocabulary_id
-)
-
-union$invalid_reason.x = ifelse(
-  is.na(union$invalid_reason.x),
-  0,
-  union$invalid_reason.x
-)
-
-union$concept_synonym_name = ifelse(
-  is.na(union$concept_synonym_name),
-  0,
-  union$concept_synonym_name
-  )
-
-
-# NA 들어있는 행 삭제
-IncompleteData <- na.omit(union)
-
-# invalid_reason NA
-colnames(IncompleteData)
-names(IncompleteData)<- c("source_domain_id", "source_code", "source_concept_id", "source_vocabulary_id",  "source_code_description", "target_concept_id", "target_vocabulary_id", "valid_start_date", "valid_end_date", "invalid_reason", "seq", "source_concept_class_id")
-
-
-# 조인키 열이름 맞추기
-names(concept)
-names(concept) <- c("target_concept_id", "concept_name", "domain_id", "vocabulary_id", "concept_class_id", "standard_concept", "concept_code", "valid_start_date", "valid_end_date",   "invalid_reason")
-
-
-# target_concept_id로 조인
-IncompleteData$target_concept_id <- as.character(IncompleteData$target_concept_id)
-concept$target_concept_id <- as.character(concept$target_concept_id)
-union1 <- left_join(IncompleteData, concept, by = "target_concept_id")
-
-IncompleteData$target_vocabulary_id <- union1$vocabulary_id
-IncompleteData$target_code_description <- union1$concept_name
-IncompleteData$target_domain_id <- union1$domain_id
-
-# 열 순서 변경(concept_class_id, vocabulary_id, source_concept_domain)
-IncompleteData <- IncompleteData %>% relocate(source_concept_class_id, .before = source_vocabulary_id) 
-IncompleteData <- IncompleteData %>% relocate(source_domain_id, .after = source_vocabulary_id)
-IncompleteData <- IncompleteData %>% relocate(target_domain_id, .after = target_vocabulary_id)
-IncompleteData <- IncompleteData %>% relocate(target_code_description, .after = target_domain_id)
-IncompleteData <- IncompleteData %>% relocate(concept_synonym_name, .before = source_code_description) 
-
-# concept_id 변경과 변경없음 합체
-conMap <- rbind(IncompleteData, CompleteData)
-
-
-# NA값 정리
-conMap$source_concept_id <- replace(conMap$source_concept_id,conMap$source_concept_id == 0, NA)
-conMap$target_concept_id <- replace(conMap$target_concept_id,conMap$target_concept_id == 0, NA)
-conMap$concept_synonym_name <- replace(conMap$concept_synonym_name,conMap$concept_synonym_name == 0, NA)
-conMap$source_concept_class_id <- replace(conMap$source_concept_class_id, conMap$source_concept_class_id == 0, NA)
-conMap$source_vocabulary_id <- replace(conMap$source_vocabulary_id,conMap$source_vocabulary_id == "None", NA)
-conMap$source_domain_id <- replace(conMap$source_domain_id,conMap$source_domain_id == "Metadata", NA)
-conMap$target_domain_id <- replace(conMap$target_domain_id,conMap$target_domain_id == "Metadata", NA)
-
-CompleteData <- CompleteData[CompleteData$target_vocabulary_id == "SNOMED",]
-
-x<- IncompleteData[IncompleteData$target_vocabulary_id != "None",]
-
-readr::write_csv(x = conMap, file = file.path(dataFolder, "SourceToConceptMap_procedure2022.11.09.csv"),  na = "")
-readr::write_csv(x = CompleteData, file = file.path(dataFolder, "CompleteMapping_procedure2022.11.09.csv"),  na = "")
-readr::write_csv(x = IncompleteData, file = file.path(dataFolder, "IncompleteMapping_procedure2022.11.09.csv"),  na = "")
 
